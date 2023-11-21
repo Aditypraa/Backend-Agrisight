ff030c19098098df2015393eca0ab4ef29c0348d
Aditya Pratama <aditypraa@gmail.com>
2023-11-22 01:59:42 +0700

    (feat/login) : Config JSON

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
-console.log("stagging...");
+import dotenv from "dotenv";
+dotenv.config();
+import express from "express";
+import cors from "cors";
+import connectDB from "./config/connectdb.js";
+
+const app = express();
+const port = process.env.PORT;
+const DATABASE_URL = process.env.DATABASE_URL;
+
+// CORS Policy
+app.use(cors());
+
+// DATABASE Connection
+connectDB(DATABASE_URL);
+
+// JSON
+app.use(express.json());
+
+app.listen(port, () => {
+  console.log(`Server listening on http://localhost:${port}`);
+});
