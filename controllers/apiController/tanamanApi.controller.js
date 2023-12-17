b31dd80c90cb0bac5dbf86f92e2b27eee8e60e1d
Aditya Pratama <aditypraa@gmail.com>
2023-12-17 22:01:18 +0700

    (feat/tanamanArtikel): Add API By Name

--- controllers/apiController/tanamanApi.controller.js
+++ controllers/apiController/tanamanApi.controller.js
@@ -20,9 +20,10 @@ class TanamanControllerApi {
       res.sendStatus(500);
     }
   };
+
   static getTanamanById = async (req, res) => {
     try {
-      const tanamanId = req.params.id; // Assumes ID is in a parameter named "id"
+      const tanamanId = req.params.id;
 
       const docSnapshot = await tanamanRef.doc(tanamanId).get();
 
@@ -41,6 +42,39 @@ class TanamanControllerApi {
         success: true,
         message: `Tanaman ${tanamanId} retrieved successfully`,
         data: { tanaman },
+      });
+    } catch (error) {
+      console.log(error);
+      res.sendStatus(500);
+    }
+  };
+
+  static getTanamanByNama = async (req, res) => {
+    try {
+      const namaTanaman = req.params.nama; // Assumsi parameter nama dikirim melalui URL
+
+      // Query berdasarkan nama tanaman
+      const query = tanamanRef.where("nama", "==", namaTanaman);
+
+      const querySnapshot = await query.get();
+
+      if (querySnapshot.empty) {
+        res.status(404).json({
+          success: false,
+          message: `Tanaman dengan nama ${namaTanaman} tidak ditemukan`,
+        });
+        return;
+      }
+
+      const tanamans = [];
+      querySnapshot.forEach((doc) => {
+        tanamans.push({ id: doc.id, ...doc.data() });
+      });
+
+      res.json({
+        success: true,
+        message: `Tanaman dengan nama ${namaTanaman} berhasil diambil`,
+        data: { tanamans },
       });
     } catch (error) {
       console.log(error);
--- routes/apiRoutes/tanamanApi.routes.js
+++ routes/apiRoutes/tanamanApi.routes.js
@@ -4,5 +4,6 @@ const router = express.Router();
 
 router.get("/", TanamanControllerApi.getTanaman);
 router.get("/:id", TanamanControllerApi.getTanamanById);
+router.get("/by-nama/:nama", TanamanControllerApi.getTanamanByNama);
 
 module.exports = router;
